a2f24eb1534972d517c6a862dbf8b49251e603b4
dz2244 <dz2244@bs.amalnet.k12.il>
2024-01-16 17:02:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/exc0607/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.exc0607;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton imbu ;
+    ImageView image ;
+    Random rnd = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imbu = findViewById(R.id.imbu);
+        image = findViewById(R.id.image);
+    }
+
+    public void ace(View view) {
+        int rand = rnd.nextInt(3)+1;
+        if (rand == 1) {
+            image.setImageResource(R.drawable.smurf1);
+            imbu.setImageResource(R.drawable.one);
+        }
+        if (rand == 2) {
+            image.setImageResource(R.drawable.smurf2);
+            imbu.setImageResource(R.drawable.two);
+        }
+        if (rand == 3) {
+            image.setImageResource(R.drawable.spookyscary);
+            imbu.setImageResource(R.drawable.three);
+        }
+
+
+
+    }
+}
