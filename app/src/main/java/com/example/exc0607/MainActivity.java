4a1ebcbde2f0b7391e24585b5f9f07cc494927b3
dz2244 <dz2244@bs.amalnet.k12.il>
2024-01-16 17:04:33 +0200

    commit2

--- app/src/main/java/com/example/exc0607/MainActivity.java
+++ app/src/main/java/com/example/exc0607/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         if (rand == 3) {
             image.setImageResource(R.drawable.spookyscary);
             imbu.setImageResource(R.drawable.three);
+
         }
 
 
